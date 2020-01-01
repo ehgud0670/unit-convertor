73b3e12ff308f9c61bb7cc179079ec69c97052ab
ehgud0670 <kimdo2297@gmail.com>
2020-01-01 22:10:30 +0900

    Add : 함수 구현

--- UnitConvertor/main.swift
+++ UnitConvertor/main.swift
@@ -5,11 +5,34 @@
 //  Created by kimdo2297 on 25/12/2019.
 //  Copyright © 2019 Jason. All rights reserved.
 //
-
 import Foundation
 
-var someCentiMeter : Int = 120
-var someMeter : Double = 1.86
+func convertLength(lengthStr : String) -> String {
+    let operand : Double = 100
+    let meter : String = "m"
+    let centimeter : String = "cm"
+    var hasSuffix : Bool = false
+    hasSuffix = lengthStr.hasSuffix(meter)
+    if(hasSuffix){
+        let start = lengthStr.index(lengthStr.startIndex, offsetBy: 0)
+        hasSuffix = lengthStr.hasSuffix(centimeter)
+        if(hasSuffix){
+            let end = lengthStr.index(lengthStr.endIndex, offsetBy: -2)
+            let range = start ..< end
+            if let num = Double(lengthStr[range]){
+                return  String(num / operand) + meter
+            }
+        } else {
+            let end = lengthStr.index(lengthStr.endIndex, offsetBy: -1)
+            let range = start ..< end
+            if let num = Double(lengthStr[range]){
+                return String(num * operand) + centimeter
+            }
+        }
+    }
+    return "error"
+}
 
-print("\(Double(someCentiMeter)/100)m")
-print("\(Int(someMeter*100))cm")
+print(convertLength(lengthStr: "1.86m"))
+print(convertLength(lengthStr: "186cm"))
+print(convertLength(lengthStr: "186"))
