20a959da3cd192846110d981ce1f00c24c06635c
ehgud0670 <kimdo2297@gmail.com>
2020-01-01 23:38:41 +0900

    Add : 인치길이 변환 기능 구현

--- UnitConvertor/main.swift
+++ UnitConvertor/main.swift
@@ -9,12 +9,43 @@ import Foundation
 
 let meter : String = "m"
 let centi : String = "cm"
-let operand : Double = 100
+
+let HUNDRED : Double = 100
+func convertCentiToMeter(lengthStr : String) -> String {
+    let start = lengthStr.index(lengthStr.startIndex, offsetBy: 0)
+    let end = lengthStr.index(lengthStr.endIndex, offsetBy: -2)
+    let range = start ..< end
+    if let num = Double(lengthStr[range]){
+        return  String(num / HUNDRED) + meter
+    }
+    return ""
+}
 
 func convertMeterToCenti(lengthStr : String) -> String {
-    
     let start = lengthStr.index(lengthStr.startIndex, offsetBy: 0)
     let end = lengthStr.index(lengthStr.endIndex, offsetBy: -1)
+    let range = start ..< end
+    if let num = Double(lengthStr[range]){
+        return String(num * HUNDRED) + centi
+    }
+    return ""
+}
+
+let inch : String = "inch"
+let operand : Double = 2.54
+func convertCentiToInch(lengthStr : String) -> String {
+    let start = lengthStr.index(lengthStr.startIndex, offsetBy: 0)
+    let end = lengthStr.index(lengthStr.endIndex, offsetBy: -2)
+    let range = start ..< end
+    if let num = Double(lengthStr[range]){
+        return String(num / operand) + inch
+    }
+    return ""
+}
+
+func convertInchToCenti(lengthStr : String) -> String {
+    let start = lengthStr.index(lengthStr.startIndex, offsetBy: 0)
+    let end = lengthStr.index(lengthStr.endIndex, offsetBy: -4)
     let range = start ..< end
     if let num = Double(lengthStr[range]){
         return String(num * operand) + centi
@@ -22,31 +53,60 @@ func convertMeterToCenti(lengthStr : String) -> String {
     return ""
 }
 
-func convertCentiToMeter(lengthStr : String) -> String {
-    let start = lengthStr.index(lengthStr.startIndex, offsetBy: 0)
-    let end = lengthStr.index(lengthStr.endIndex, offsetBy: -2)
-    let range = start ..< end
-    if let num = Double(lengthStr[range]){
-        return  String(num / operand) + meter
-    }
-    return ""
+func convertInchToMeter(temp : String) -> String {
+    let lengthStr = convertCentiToMeter(lengthStr: convertInchToCenti(lengthStr: temp))
+    return lengthStr
 }
 
+func convertMeterToInch(temp : String) -> String {
+    let lengthStr = convertCentiToInch(lengthStr: convertMeterToCenti(lengthStr: temp))
+    return lengthStr
+}
 
-for _ in 0 ..< 3 {
-    print("값을 입력해주세요. cm or m ( example : 186cm , 1.86m )")
-    if let inputValue = readLine(){
-        var hasSuffix : Bool = false
-        hasSuffix = inputValue.hasSuffix(meter)
-        if(hasSuffix){
-            hasSuffix = inputValue.hasSuffix(centi)
-            if(hasSuffix){
-                print(convertCentiToMeter(lengthStr: inputValue))
-            } else {
-                print(convertMeterToCenti(lengthStr: inputValue))
+print("값을 입력해주세요. cm or m ( example : 186cm , 1.86m )")
+if let inputValue = readLine(){
+    let tokens = inputValue.components(separatedBy: " ")
+    if(tokens[0].hasSuffix("cm")){
+        if(tokens.count == 2) {
+            switch tokens[1] {
+            case meter:
+                print(convertCentiToMeter(lengthStr: tokens[0]))
+            case inch:
+                print(convertCentiToInch(lengthStr: tokens[0]))
+            default:
+                print("")
             }
         } else {
-            print("Error")
+            print(convertCentiToMeter(lengthStr: tokens[0]))
         }
+    } else if (tokens[0].hasSuffix("m")){
+        if(tokens.count == 2) {
+            switch tokens[1] {
+            case centi:
+                print(convertMeterToCenti(lengthStr: tokens[0]))
+            case inch:
+                print(convertMeterToInch(temp: tokens[0]))
+            default:
+                print("")
+            }
+        } else {
+            print(convertMeterToCenti(lengthStr: tokens[0]))
+        }
+    } else if (tokens[0].hasSuffix("inch")){
+        if(tokens.count == 2) {
+            switch tokens[1] {
+            case centi:
+                print(convertInchToCenti(lengthStr: tokens[0]))
+            case meter:
+                print(convertInchToMeter(temp: tokens[0]))
+            default:
+                print("")
+            }
+        } else {
+            print(convertInchToCenti(lengthStr: tokens[0]))
+        }
+    } else {
+        print("지원하지 않는 단위입니다.")
     }
 }
+
